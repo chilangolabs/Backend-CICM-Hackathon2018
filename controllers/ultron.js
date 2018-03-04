293328af3626eea53aa7aacb30b0fb475311a0fa
Rodrigo Moreno <moreno.rdr@gmail.com>
2018-03-04 13:49:47 -0600

    + Added start command handler

--- controllers/ultron.js
+++ controllers/ultron.js
@@ -5,6 +5,15 @@ let bot = new Telegram(process.env.TELEGRAM_BOT_TOKEN)
 module.exports = router => {
   router.post('/', async (req, res, next) => {
     if (req.body.message) {
+      // Command start
+      if (req.body.message.entities && Array.isArray(req.body.message.entities)) {
+        if (req.body.message.entities.find(entity => entity.type === 'bot_command').length > 0) {
+          if (req.body.message.text.includes('start')) {
+            // TODO
+          }
+        }
+      }
+
       const chatId = req.body.message.chat.id
       let text = 'Holi Kalpoli'
 
